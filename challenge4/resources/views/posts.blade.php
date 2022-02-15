598e526fb426f8deb9827617718ef6025fd9803d
denuApp <denise@lightit.io>
2022-02-15 14:49:45 -0300

    installing laravel and creating challenge4

---
+++ challenge4/resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+
+<title>My Blog</title>
+
+<link rel='stylesheet' href='/app.css'>
+
+<body>
+
+    <article>
+        <h1><a href='/post/my-first-post'>My First Post</a></h1>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt officiis nemo quaerat aperiam aspernatur. Numquam eos repellendus necessitatibus nam similique in! Molestiae, eum a. Atque eos deleniti aliquid quia consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, modi itaque dolorum quibusdam corrupti atque unde aliquid soluta libero, dolore quisquam magnam laudantium, qui sint ipsam voluptatibus similique delectus vero. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, aliquid voluptates repellat, libero incidunt nam sit doloribus at laboriosam voluptatem recusandae accusamus labore. Ipsam, rerum consectetur! Magni culpa recusandae debitis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quaerat tempore nobis vel fuga pariatur voluptatibus, facilis dignissimos ab. Dolores, dolorem dicta. Iure eligendi, delectus quam sed sapiente unde omnis!
+        </p>
+    </article>
+
+    <article>
+    <h1><a href='/post/my-second-post'>My Second Post</a></h1>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt officiis nemo quaerat aperiam aspernatur. Numquam eos repellendus necessitatibus nam similique in! Molestiae, eum a. Atque eos deleniti aliquid quia consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, modi itaque dolorum quibusdam corrupti atque unde aliquid soluta libero, dolore quisquam magnam laudantium, qui sint ipsam voluptatibus similique delectus vero. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, aliquid voluptates repellat, libero incidunt nam sit doloribus at laboriosam voluptatem recusandae accusamus labore. Ipsam, rerum consectetur! Magni culpa recusandae debitis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quaerat tempore nobis vel fuga pariatur voluptatibus, facilis dignissimos ab. Dolores, dolorem dicta. Iure eligendi, delectus quam sed sapiente unde omnis!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href='/post/my-third-post'>My Third Post</a></h1>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt officiis nemo quaerat aperiam aspernatur. Numquam eos repellendus necessitatibus nam similique in! Molestiae, eum a. Atque eos deleniti aliquid quia consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, modi itaque dolorum quibusdam corrupti atque unde aliquid soluta libero, dolore quisquam magnam laudantium, qui sint ipsam voluptatibus similique delectus vero. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, aliquid voluptates repellat, libero incidunt nam sit doloribus at laboriosam voluptatem recusandae accusamus labore. Ipsam, rerum consectetur! Magni culpa recusandae debitis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quaerat tempore nobis vel fuga pariatur voluptatibus, facilis dignissimos ab. Dolores, dolorem dicta. Iure eligendi, delectus quam sed sapiente unde omnis!
+        </p>
+    </article>
+</body>
