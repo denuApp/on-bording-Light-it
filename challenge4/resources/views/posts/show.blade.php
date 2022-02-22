eac8f7139d876076d63c9584075955248ce88d2e
denuApp <denise@lightit.io>
2022-02-22 10:16:16 -0300

    termine con los videitos

--- challenge4/resources/views/posts/show.blade.php
+++ challenge4/resources/views/posts/show.blade.php
@@ -52,6 +52,32 @@
                            {!! $post->body !!}
                         </div>
                     </div>
+
+                    <section class="col-span-8 col-start-5 mt-10">
+                        <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+                            <div class="flex-shrink-0">
+                                <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+                            </div>
+
+                            <div>
+                                <header class="mb-4">
+                                    <h3 class="font-bold">John Doe</h3>
+                                    <p class="text-xs">
+                                        Posted
+                                        <time>8 months ago</time>
+                                    </p>
+                                </header>
+
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sit architecto fuga eos atque. Maiores mollitia in illum, repellat tempore dolorem, corporis aperiam, quis earum atque porro laboriosam itaque praesentium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid exercitationem suscipit hic consectetur quod maxime dolor iure itaque? Tempora voluptatum aperiam tenetur dignissimos in quis repudiandae voluptatibus recusandae totam praesentium!
+                                </p>
+                            </div>
+
+                            <section class="col-span-8 col-start-5 mt-10">
+
+                            </section>
+                        </article>
+                    </section>
                 </article>
             </main>
         </section>
