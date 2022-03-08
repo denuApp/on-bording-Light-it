c3f6bd50d1d21fb42a14e05b6852678aa4296806
denuApp <denise@lightit.io>
2022-03-08 18:06:14 -0300

    i didnt change anything

--- challengeFlights/app/Models/Disponibility.php
+++ challengeFlights/app/Models/Disponibility.php
@@ -13,7 +13,7 @@ class Disponibility extends Model
 
     public function city()
     {
-        return $this->hasMany(City::class, 'id','city_id');
+        return $this->hasMany(City::class, 'id', 'city_id');
     }
 
     public function airline()
